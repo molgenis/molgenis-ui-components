db0e9fe2dbc44cc54b1eaf57219638c8b4a86511
Max Postema <m.e.postema@rug.nl>
2019-12-13 16:02:27 +0100

    test: selenium config

---
+++ nightwatch.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  test_settings: {
+    selenium: {
+      launch_url: 'http://ondemand.saucelabs.com:80',
+      start_process: true,
+      host: 'ondemand.saucelabs.com',
+      port: 80,
+      username: process.env.SAUCE_CRED_USR,
+      access_key: process.env.SAUCE_CRED_PSW
+    }
+  }
+}
