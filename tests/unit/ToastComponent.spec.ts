def1ce162ff3d7720a8d203cca4cb826e8a1968d
Max Postema <m.e.postema@rug.nl>
2019-12-24 12:44:45 +0100

    test: add property change test

--- tests/unit/ToastComponent.spec.ts
+++ tests/unit/ToastComponent.spec.ts
@@ -1,5 +1,6 @@
 import { shallowMount } from '@vue/test-utils'
 import ToastComponent from '@/components/ToastComponent.vue'
+import Vue from 'vue'
 
 describe('ToastComponent.vue', () => {
   it('shows information', () => {
@@ -9,9 +10,7 @@ describe('ToastComponent.vue', () => {
     expect(wrapper.find('.card-text').text()).toBe('message')
     expect(wrapper.find('.card').classes()).toContain('bg-info')
   })
-})
 
-describe('ToastComponent.vue', () => {
   it('can hide after some set time', (done) => {
     const wrapper = shallowMount(ToastComponent, { propsData: { value: [{ message: 'message', title: 'title', type: 'info', timeout: 10 }] } })
     expect(wrapper.emitted()).toEqual({})
@@ -19,5 +18,12 @@ describe('ToastComponent.vue', () => {
       expect(wrapper.emitted().input).toEqual([[[]]])
       done()
     }, 15)
+  })
+
+  it('listens to property changes', () => {
+    const wrapper = shallowMount(ToastComponent, { propsData: { value: [{ message: 'message', title: 'title' }] } })
+    expect(wrapper.find('.card-text').text()).toBe('message')
+    wrapper.setProps({ value: [{ message: 'changed', title: 'title', type: 'info' }] })
+    expect(wrapper.find('.card-text').text()).toBe('changed')
   })
 })
